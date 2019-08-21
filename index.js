d1a6cc69dd517b53bc725857226a69b5032d607e
cbrand125 <cbrand@talentpath.com>
2019-08-21 10:53:52 -0700

    add cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const router = require('./middleware/routes');
 const logger = require('./middleware/logger');
 const errorHandler = require('./middleware/errorhandler');
@@ -6,9 +7,12 @@ const errorHandler = require('./middleware/errorhandler');
 const app = express();
 const port = 5000;
 
+app.use(cors());
 app.use(express.json());
 app.use(logger);
 app.use(router);
 app.use(errorHandler);
 
-app.listen(process.env.PORT || port, () => console.log(`Snippets listening on port ${port}!`));
+app.listen(process.env.PORT || port, () =>
+  console.log(`Snippets listening on port ${port}!`)
+);
