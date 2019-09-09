2f4f710d1a3644e3041b028758d47838f2b686a2
cbrand125 <cbrand@talentpath.com>
2019-09-09 11:13:04 -0700

    use database url

--- db/index.js
+++ db/index.js
@@ -1,8 +1,6 @@
 require('dotenv').config();
 const pg = require('pg');
 
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE } = process.env;
-const connectionString = `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
-const pool = new pg.Pool({ connectionString, ssl: true });
+const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
 
 module.exports = pool;
